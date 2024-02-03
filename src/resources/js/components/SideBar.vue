d4b0a1f3921a42266e040ed7983745383aca51e3
Kamechan516 <kamechan5062@gmail.com>
2024-02-03 23:28:52 +0900

    Vueのコンポーネントを追加

---
+++ src/resources/js/components/SideBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="sidebar">
+    <!-- Sidebar content goes here -->
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SideBar",
+  // Component properties and methods go here
+}
+</script>
+
+<style scoped>
+</style>
